7f60188ccab188fb656f8297159fa64eba393031
Vladimir franca de sousa <vfrancad@gmail.com>
2023-11-21 10:15:22 +0100

    Add files via upload

---
+++ velo/variable.tf
@@ -0,0 +1,24 @@
+# --- root/variable ---
+variable "vco_address" {
+  type = string
+}
+
+variable "vco_username" {
+  type = string
+}
+
+variable "vco_password" {
+  type = string
+}
+
+variable "vco_url" {
+  type = string
+}
+
+variable "edge_profile" {
+  type = string
+}
+
+variable "cidr_block" {
+  type = string
+}
